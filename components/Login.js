471390b31817742f0f2fdd8a59589a470763d2de
hadizareoriginal <hadizareoriginal@gmail.com>
2020-01-13 01:02:34 +0330

    create components & componentStyle folder and first init Login.js & login.css and add login to App.js

---
+++ components/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../componentStyle/login.css";
+
+function Login() {
+  return (
+    <div className="login">
+      <p>this is login</p>
+    </div>
+  );
+}
+
+export default Login;
