becd9834e025ed228a392d098df671863654a169
hadizareoriginal <hadizareoriginal@gmail.com>
2020-01-13 02:22:11 +0330

    some change to login

--- components/Login.js
+++ components/Login.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "../componentStyle/login.css";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
 
 function Login() {
   return (
-    <div className="login">
-      <p>this is login</p>
+    <div id="login">
+      {/* <div id="description">
+        <p>login furom</p>
+      </div> */}
+      <Grid container spacing={3} id="box">
+        <Grid item xs={6}>
+          <Paper id="description">xs=6</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper id="description">xs=6</Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 }
